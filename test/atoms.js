004551c34323ca509576026ca32ffd80ad8b8e18
ajlopez <webmaster@ajlopez.com>
2018-11-30 19:22:21 -0300

    Is atom

--- test/atoms.js
+++ test/atoms.js
@@ -14,3 +14,13 @@ exports['atom to string'] = function (test) {
     test.strictEqual(atom.toString(), 'foo');
 };
 
+exports['is atom'] = function (test) {
+    var atom = atoms.atom('foo');
+
+    test.ok(atoms.isAtom(atom));
+    test.ok(!atoms.isAtom(null));
+    test.ok(!atoms.isAtom(42));
+    test.ok(!atoms.isAtom("foo"));
+    test.ok(!atoms.isAtom({}));
+};
+
