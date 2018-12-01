20464e155a648084b4b05270326b7c7ee9f7f7c9
ajlopez <webmaster@ajlopez.com>
2018-12-01 15:50:56 -0300

    Evaluate tuple with bound variables

--- test/tuples.js
+++ test/tuples.js
@@ -58,3 +58,27 @@ exports['evaluate tuple with three unbound variables'] = function (test) {
     test.equal(result.element(2), varz);
 };
 
+exports['evaluate tuple with three bound variables'] = function (test) {
+    var context = contexts.context();
+    var varx = variables.variable('X');
+    var vary = variables.variable('Y');
+    var varz = variables.variable('Z');
+    
+    var tuple = tuples.tuple([varx, vary, varz]);
+
+    context.bind(varx, 1);
+    context.bind(vary, 4);
+    context.bind(varz, 9);
+    
+    var result = tuple.evaluate(context);
+    
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.ok(tuples.isTuple(result));
+
+    test.equal(result.size(), 3);
+    test.equal(result.element(0), 1);
+    test.equal(result.element(1), 4);
+    test.equal(result.element(2), 9);
+};
+
