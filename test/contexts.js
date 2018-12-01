7fe382f084a4edb4602bb6dc9787f7cba1b7b2dd
ajlopez <webmaster@ajlopez.com>
2018-12-01 11:48:16 -0300

    Cannot bind variable twice

--- test/contexts.js
+++ test/contexts.js
@@ -25,6 +25,23 @@ exports['bind and resolve variable using integer value'] = function (test) {
     test.equal(context.resolve(varx), 42);
 };
 
+exports['cannot bind a variable twice'] = function (test) {
+    var varx = variables.variable('X');
+    var context = contexts.context();
+    
+    context.bind(varx, 42);
+    
+    try {
+        context.bind(varx, 144);
+    }
+    catch (ex) {
+        test.equal(ex, 'Error: already bound variable');
+        return;
+    }
+    
+    test.fail('Test failed');
+};
+
 exports['bind and resolve variable using variable value'] = function (test) {
     var varx = variables.variable('X');
     var vary = variables.variable('Y');
