b1c2b921a5f3a298d593a525dab5013d7ce0b12f
ajlopez <webmaster@ajlopez.com>
2018-12-01 19:14:21 -0300

    Send and receive messages

--- test/processes.js
+++ test/processes.js
@@ -8,3 +8,49 @@ exports['create process'] = function (test) {
     test.equal(typeof process, 'object');
 };
 
+exports['send and receive message'] = function (test) {
+    test.async();
+    
+    var process = processes.process();
+    
+    process.send(42);
+    
+    process.receive(function (message) {
+        test.equal(message, 42);
+        test.done();
+    });
+};
+
+exports['send and receive two messages'] = function (test) {
+    test.async();
+    
+    var process = processes.process();
+    
+    process.send(42);
+    process.send(144);
+    
+    process.receive(function (message) {
+        test.equal(message, 42);
+        test.equal(process.size(), 0);
+    });
+
+    process.receive(function (message) {
+        test.equal(message, 144);
+        test.equal(process.size(), 0);
+        test.done();
+    });
+};
+
+exports['receive and send message'] = function (test) {
+    test.async();
+    
+    var process = processes.process();
+    
+    process.receive(function (message) {
+        test.equal(message, 42);
+        test.done();
+    });
+    
+    process.send(42);
+};
+
