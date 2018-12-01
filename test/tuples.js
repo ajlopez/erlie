14867a041d9b5185f1dbf9aa104b14b66e1b59a5
ajlopez <webmaster@ajlopez.com>
2018-12-01 15:47:46 -0300

    Tuple evaluation

--- test/tuples.js
+++ test/tuples.js
@@ -1,5 +1,6 @@
 
 var tuples = require('../lib/tuples');
+var contexts = require('../lib/contexts');
 
 exports['create empty tuple'] = function (test) {
     var tuple = tuples.tuple([]);
@@ -10,6 +11,18 @@ exports['create empty tuple'] = function (test) {
 };
 
 exports['create tuple with three elements'] = function (test) {
+    var tuple = tuples.tuple([1, 4, 9]);
+    
+    test.ok(tuple);
+    test.equal(typeof tuple, 'object');
+    test.equal(tuple.size(), 3);
+    test.equal(tuple.element(0), 1);
+    test.equal(tuple.element(1), 4);
+    test.equal(tuple.element(2), 9);
+};
+
+exports['evaluate tuple with three constants elements'] = function (test) {
+    var context = contexts.context();
     var tuple = tuples.tuple([1, 4, 9]);
     
     test.ok(tuple);
