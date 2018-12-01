5292fcde1a3679e1f37c52d302f5d4a46ebb139f
ajlopez <webmaster@ajlopez.com>
2018-12-01 11:43:46 -0300

    Bind variable to a variable

--- test/contexts.js
+++ test/contexts.js
@@ -16,12 +16,23 @@ exports['resolve unbound variable'] = function (test) {
     test.equal(context.resolve(varx), varx);
 };
 
-exports['bind and resolve variable'] = function (test) {
+exports['bind and resolve variable using integer value'] = function (test) {
     var varx = variables.variable('X');
     var context = contexts.context();
     
     context.bind(varx, 42);
     
     test.equal(context.resolve(varx), 42);
+};
+
+exports['bind and resolve variable using variable value'] = function (test) {
+    var varx = variables.variable('X');
+    var vary = variables.variable('Y');
+    
+    var context = contexts.context();
+    
+    context.bind(varx, vary);
+    
+    test.equal(context.resolve(varx), vary);
 };
 
