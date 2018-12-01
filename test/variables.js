cfc32bfa8ae4e990339f24f96802595c581c6420
ajlopez <webmaster@ajlopez.com>
2018-12-01 12:04:09 -0300

    Evaluate unbound variable to itself

--- test/variables.js
+++ test/variables.js
@@ -1,28 +1,29 @@
 
 var variables = require('../lib/variables');
 var atoms = require('../lib/atoms');
+var contexts = require('../lib/contexts');
 
 exports['create variable'] = function (test) {
-    var variable = variables.variable('foo');
+    var variable = variables.variable('Foo');
     
     test.ok(variable);
     test.strictEqual(typeof variable, 'object');
 };
 
 exports['variable to string'] = function (test) {
-    var variable = variables.variable('foo');
+    var variable = variables.variable('Foo');
     
-    test.strictEqual(variable.toString(), 'foo');
+    test.strictEqual(variable.toString(), 'Foo');
 };
 
 exports['variable name'] = function (test) {
-    var variable = variables.variable('foo');
+    var variable = variables.variable('Foo');
     
-    test.strictEqual(variable.name(), 'foo');
+    test.strictEqual(variable.name(), 'Foo');
 };
 
 exports['is variable'] = function (test) {
-    var variable = variables.variable('foo');
+    var variable = variables.variable('Foo');
 
     test.ok(variables.isVariable(variable));
     test.ok(!variables.isVariable(null));
@@ -30,5 +31,12 @@ exports['is variable'] = function (test) {
     test.ok(!variables.isVariable(42));
     test.ok(!variables.isVariable("foo"));
     test.ok(!variables.isVariable({}));
+};
+
+exports['evaluate unbound variable to itself'] = function (test) {
+    var variable = variables.variable('X');
+    var context = contexts.context();
+    
+    test.equal(variable.evaluate(context), variable);
 };
 
