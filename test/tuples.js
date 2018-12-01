7b6d4716c5f2c0c1b6528a04be39bd872eabda18
ajlopez <webmaster@ajlopez.com>
2018-12-01 15:49:47 -0300

    Tuple evaluation additional test

--- test/tuples.js
+++ test/tuples.js
@@ -1,6 +1,7 @@
 
 var tuples = require('../lib/tuples');
 var contexts = require('../lib/contexts');
+var variables = require('../lib/variables');
 
 exports['create empty tuple'] = function (test) {
     var tuple = tuples.tuple([]);
@@ -24,12 +25,36 @@ exports['create tuple with three elements'] = function (test) {
 exports['evaluate tuple with three constants elements'] = function (test) {
     var context = contexts.context();
     var tuple = tuples.tuple([1, 4, 9]);
+
+    var result = tuple.evaluate(context);
     
-    test.ok(tuple);
-    test.equal(typeof tuple, 'object');
-    test.equal(tuple.size(), 3);
-    test.equal(tuple.element(0), 1);
-    test.equal(tuple.element(1), 4);
-    test.equal(tuple.element(2), 9);
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.ok(tuples.isTuple(result));
+
+    test.equal(result.size(), 3);
+    test.equal(result.element(0), 1);
+    test.equal(result.element(1), 4);
+    test.equal(result.element(2), 9);
+};
+
+exports['evaluate tuple with three unbound variables'] = function (test) {
+    var context = contexts.context();
+    var varx = variables.variable('X');
+    var vary = variables.variable('Y');
+    var varz = variables.variable('Z');
+    
+    var tuple = tuples.tuple([varx, vary, varz]);
+
+    var result = tuple.evaluate(context);
+    
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.ok(tuples.isTuple(result));
+
+    test.equal(result.size(), 3);
+    test.equal(result.element(0), varx);
+    test.equal(result.element(1), vary);
+    test.equal(result.element(2), varz);
 };
 
