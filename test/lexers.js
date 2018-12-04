3763659a733db259784dcb3bacebb9d7f733cff3
ajlopez <webmaster@ajlopez.com>
2018-12-04 08:04:00 -0300

    Lexer first token

--- test/lexers.js
+++ test/lexers.js
@@ -1,5 +1,6 @@
 
 var lexers = require('../lib/lexers');
+var TokenType = lexers.TokenType;
 
 exports['create lexer as object'] = function (test) {
     var lexer = lexers.lexer('foo');
@@ -8,3 +9,12 @@ exports['create lexer as object'] = function (test) {
     test.equal(typeof lexer, 'object');
 };
 
+exports['first token'] = function (test) {
+    var lexer = lexers.lexer('foo');
+  
+    var token = lexer.nextToken();
+    
+    test.ok(token);
+    test.equal(token.value, 'foo');
+    test.equal(token.type, TokenType.Name);
+};
