3e71306e1ca3ab8cbe823fac7c5e3f4d3809ac7d
ajlopez <webmaster@ajlopez.com>
2018-12-12 10:06:24 -0300

    Parse variable

--- test/parsers.js
+++ test/parsers.js
@@ -1,5 +1,8 @@
 
 var parsers = require('../lib/parsers');
+var contexts = require('../lib/contexts');
+var atoms = require('../lib/atoms');
+var variables = require('../lib/variables');
 
 exports['create parser as object'] = function (test) {
     var parser = parsers.parser('foo');
@@ -24,5 +27,21 @@ exports['parse atom'] = function (test) {
     
     test.ok(result);
     test.strictEqual(result.evaluate(), result);
+    test.ok(atoms.isAtom(result));
+};
+
+exports['parse variable'] = function (test) {
+    var parser = parsers.parser('X');
+    var context = contexts.context();
+    var varx = variables.variable('X');
+    
+    context.bind(varx, 42);
+    
+    var result = parser.parse();
+    
+    test.ok(result);
+    test.strictEqual(result.evaluate(context), 42);
+    test.ok(variables.isVariable(result));
+    test.ok(result.equals(varx));
 };
 
