a58233c2d5b9dacc807f31aef62c35defc8ecea3
ajlopez <webmaster@ajlopez.com>
2018-12-25 11:48:00 -0300

    List isList

--- test/lists.js
+++ test/lists.js
@@ -1,5 +1,8 @@
 
 var lists = require('../lib/lists');
+var tuples = require('../lib/tuples');
+var atoms = require('../lib/atoms');
+var variables = require('../lib/variables');
 
 exports['create list'] = function (test) {
     var list = lists.list(42);
@@ -12,5 +15,20 @@ exports['get head'] = function (test) {
     var list = lists.list(42);
     
     test.equal(list.head(), 42);
+};
+
+exports['is list'] = function (test) {
+    var list1 = lists.list(42);
+    var list2 = lists.list(1, lists.list(2));
+    
+    test.ok(lists.isList(list1));
+    test.ok(lists.isList(list2));
+   
+    test.ok(!lists.isList(null));
+    test.ok(!lists.isList(42));
+    test.ok(!lists.isList("foo"));
+    test.ok(!lists.isList(atoms.atom('adam')));
+    test.ok(!lists.isList(variables.variable('X')));
+    test.ok(!lists.isList(tuples.tuple([1, 2, 3])));
 };
 
